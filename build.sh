a86e6b959efe54ddf8f602f1350eaea25b6a9cfd
Peter Urbak <peter@dragonwasrobot.com>
2026-04-06 00:24:03 +0200

    fix: corrects wrong filename for iosevka assets

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ cp -r ./node_modules/@fortawesome/fontawesome-free/webfonts static/fontawesome/
 ## Iosevka
 mkdir -p static/iosevka/files
 cp ./node_modules/@fontsource/iosevka/200.css static/iosevka/font.css
-cp ./node_modules/@fontsource/iosevka/files/iosevka-latin-200-italic.woff static/iosevka/files/
-cp ./node_modules/@fontsource/iosevka/files/iosevka-latin-200-italic.woff2 static/iosevka/files/
+cp ./node_modules/@fontsource/iosevka/files/iosevka-latin-200-normal.woff static/iosevka/files/
+cp ./node_modules/@fontsource/iosevka/files/iosevka-latin-200-normal.woff2 static/iosevka/files/
 
 echo "Done!"
